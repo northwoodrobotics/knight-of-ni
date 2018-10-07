c83418236d83649c7cfdda914bec233e967291a5
robotics <robotics@computer362.northwoodschool.local>
2018-10-07 14:38:57 -0400

    add TeleDrive command

---
+++ KnightOfNi/src/org/usfirst/frc/team9300/robot/commands/TeleDrive.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team9300.robot.commands;
+
+import org.usfirst.frc.team9300.robot.OI;
+import org.usfirst.frc.team9300.robot.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TeleDrive extends Command {
+	private Drivetrain drivetrain;
+	private XboxController controller = OI.driveController;
+	
+	private double fwd;
+	private double strafe;
+	private double rotateCW;
+	
+	public TeleDrive(Drivetrain drivetrain) {
+		this.drivetrain = drivetrain;
+		requires(drivetrain);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		drivetrain.init();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		fwd = OI.deadBand(controller.getY(Hand.kLeft));
+		strafe = OI.deadBand(controller.getX(Hand.kLeft));
+		rotateCW = OI.deadBand(controller.getX(Hand.kRight));
+		
+		drivetrain.drive(fwd, strafe, rotateCW);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
